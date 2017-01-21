fc6ed8feb6c689f23ad089ba25b85dc2ba00a1bf
squarerootfury <fury224@googlemail.com>
2017-01-21 18:40:59 +0100

    + Theme changes

--- themes/plain/post.tpl.php
+++ themes/plain/post.tpl.php
@@ -1,6 +1,8 @@
 <?php
     $markdown = new Parsedown();
 ?>
+
+<?php var_dump($post);?>
 <a href="<?php echo $meta->URL;?>post/<?php echo $post->URL;?>/"><h1><?php echo $post->Title;?></h1></a>
 Date: <?php echo $post->Date;?>
 <div>
